4035c0fdb63047704babe8121dddc41d9046c81d
amine-maazizi <amine.maazizi@ensta-paris.fr>
2026-08-15 18:56:07 +0200

    layout, some content and styling; customized the alert pop up :)

---
+++ README.md
@@ -0,0 +1,7 @@
+# Research Portfolio 
+
+This repository has been used to host my research/personal portfolio. Within it, you may find two branches representing older versions of my portfolio, versions that were vibecoded from A to Z. I decided to redo my portfolio from scratch in this branch, typing every single line of code.
+
+Since no one will read this README.md, I'm going to use it as a personal journaling page about my own reflections when it comes to employability in the field of CS. I have been motivated to rework on my portfolio after hearing this analogy that resonated with me : For your portfolio, you're building something of quality, not quantity. It's like if you wanted to be an apprentice to the best weaver in the world, they've been weaving all their life and have gotten good at their craft through continuous learning and trial and error, and you're like : Yo, please take me as an apprentice, here look at these cheap blankets I mass-produced in a factory. 
+
+A starting point in my opinion is the portfolio website, if you vibe coded your own portfolio website then how do you want a recruiter, or anyone who knows a thing or two about coding, that the projects within it are your own work?
---
+++ src/js/index.js
@@ -0,0 +1,86 @@
+async function copyToClipboard() {
+    // Get the text field
+    var button = document.getElementById("email");
+    var text = button.textContent;
+    var sanitizedText = text.replace(" [at] ", "@").replace(" (dot) ", ".");
+
+    navigator.clipboard.writeText(sanitizedText);
+
+    button.style.pointerEvents = 'none';
+
+    await customAlert("Copied to clipboard");
+
+    button.style.pointerEvents = 'auto';
+}
+
+
+const lerp = (start, end, amt) => start + (end - start) * amt;
+
+function lerpAnimation(element, start, end, duration) {
+    let startTime;
+
+    function animate(time) {
+        if (startTime === undefined)
+            startTime = time;
+
+        const elapsed = time - startTime;
+        const t = Math.min(elapsed / duration, 1);
+
+        element.style.left = lerp(start[0], end[0], t) + "px";
+        element.style.top  = lerp(start[1], end[1], t) + "px";
+
+        if (t < 1)
+            requestAnimationFrame(animate);
+    }
+
+    requestAnimationFrame(animate);
+}
+
+
+function fadeawayAnimation(element, duration) {
+    return new Promise((resolve) => {
+        let startTime;
+
+        function animate(time) {
+            if (startTime === undefined)
+                startTime = time;
+
+            const elapsed = time - startTime;
+            const t = Math.min(elapsed / duration, 1);
+
+            element.style.opacity = String(1 - t);
+
+            if (t < 1)
+                requestAnimationFrame(animate);
+            else
+                resolve();
+        }
+
+        requestAnimationFrame(animate);
+    })
+}
+
+const pixelToNumber = (px) => Number(px.slice(0, -2));
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+async function customAlert(text) {
+    var divHeight = pixelToNumber(getComputedStyle(document.documentElement).getPropertyValue('--alert-height'));
+
+    var alertDiv = document.getElementById("alert");
+    alertDiv.innerHTML = text;
+
+    var position = [pixelToNumber(getComputedStyle(alertDiv).left), pixelToNumber(getComputedStyle(alertDiv).top)];
+    
+    var start = position
+    var end = [position[0], position[1] + 5 * divHeight /  4];
+
+    lerpAnimation(alertDiv, start, end, 100);
+
+    await sleep(1000);
+
+    await fadeawayAnimation(alertDiv, 500);
+
+    alertDiv.style.left = start[0] + 'px';
+    alertDiv.style.top = start[1] + 'px';
+    alertDiv.style.opacity = '1.0';
+}
